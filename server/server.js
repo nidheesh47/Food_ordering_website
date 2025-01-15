226b2632e10821a25a7ab1868b83a09de2603cb3
nidheesh47 <nidheeshedappon0479@gmail.com>
2025-01-16 00:20:24 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const apiRouter = require("./routes");
+
+const { PORT, connectDB } = require("./config/db");
+
+const port = PORT;
+const db = connectDB;
+
+db();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api", apiRouter);
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "end point does not exist" });
+});
