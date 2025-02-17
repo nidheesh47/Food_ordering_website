2abec471cf41eaa4f59dd099929ec80ece25c368
nidheesh47 <nidheeshedappon0479@gmail.com>
2025-02-17 16:38:08 +0530

    update

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -3,16 +3,3 @@ export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
